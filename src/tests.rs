1034a6ec6ae9784d750f7b4dd223bb949f6f77f2
Kayson Shaikh <kaysonshaikh@gmail.com>
2024-04-24 21:23:59 -0400

    test epoch_to_date function

--- src/tests.rs
+++ src/tests.rs
@@ -2,10 +2,17 @@
 mod test {
     use crate::col_to_vec;
     use crate::VecType;
+    use crate ::epoch_to_date;
 
     #[test]
     fn test_col_to_vec() {
         let data: Vec<(i32, i32, i32, f64)> = vec![(1, 2, 1, 1.0), (2, 3, 2, 2.0), (3, 4, 3, 3.0)];
         assert_eq!(VecType::IntVec(vec![2, 3, 4]), col_to_vec(&data, 1), "col_to_vec function not working properly");
+    }
+
+    #[test]
+    fn test_epoch_to_date() {
+        assert_eq!((1, 1970), epoch_to_date(1.0), "epoch_to_date function not working properly");
+        assert_eq!((2, 1971), epoch_to_date(31_540_000.0 + 3_000_000.0), "epoch_to_date function not working properly");
     }
 }
