dd43bf43bc7c7ac6c5432e63e62fd24d462e2c94
Kayson Shaikh <kaysonshaikh@gmail.com>
2024-05-05 18:40:49 -0400

    test function for components_and_sizes function

--- src/tests.rs
+++ src/tests.rs
@@ -4,6 +4,7 @@ mod test {
     use crate::data_manipulation::data_manipulation::VecType;
     use crate::epoch_to_date;
     use crate::strong_ratings_only;
+    use crate::components_and_sizes;
 
     #[test]
     fn test_col_to_vec() {
@@ -21,5 +22,11 @@ mod test {
     fn test_strong_ratings_only() {
         let data: Vec<(i32, i32, i32, f64)> = vec![(1, 2, 1, 1.0), (2, 3, 2, 2.0), (3, 4, 7, 3.0), (4, 5, 8, 4.0), (5, 6, 10, 5.0)];
         assert_eq!(vec![(4, 5, 8, 4.0), (5, 6, 10, 5.0)], strong_ratings_only(&data), "strong_ratings_only function not working properly.");
+    }
+
+    #[test]
+    fn test_components_and_sizes() {
+        let data: Vec<(i32, i32, i32, f64)> = vec![(1, 2, 1, 1.0), (2, 3, 2, 2.0), (3, 4, 7, 3.0), (5, 6, 10, 5.0), (6, 7, 10, 6.0), (8, 9, 5, 7.0)];
+        assert_eq!((3, vec![4, 3, 2]), components_and_sizes(&data), "components_and_sizes function not working properly");
     }
 }
